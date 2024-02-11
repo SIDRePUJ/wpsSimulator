03edca0c336fbbd2b45e879daff7dfc0888ab7d1
Jairo Serrano <jairo.serrano@gmail.com>
2024-02-11 11:53:53 -0500

    Activados los prestamos y pagos en el banco.

--- src/main/java/org/wpsim/Bank/Guards/BankAgentGuard.java
+++ src/main/java/org/wpsim/Bank/Guards/BankAgentGuard.java
@@ -47,7 +47,7 @@ public class BankAgentGuard extends GuardBESA {
         BankAgentState state = (BankAgentState) this.agent.getState();
 
         BankMessageType messageType = bankMessage.getMessageType();
-        System.out.println("$$$ Bank " + messageType + " desde " + bankMessage.getPeasantAlias());
+        System.out.println("$$$ Bank " + messageType + " desde " + bankMessage.getPeasantAlias() + " por " + bankMessage.getAmount() + " $$$");
 
         try {
             //wpsReport.info("$ uno ");
